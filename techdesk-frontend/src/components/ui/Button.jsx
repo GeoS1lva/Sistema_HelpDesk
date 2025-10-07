63d2785b46c56955a22e4f5150c8836aa4296c58
Eduardo Zanin <108440667+eduardozaninn@users.noreply.github.com>
2025-10-07 00:29:54 -0300

    Ajustando tela de Redefinição de Senha

--- techdesk-frontend/src/components/ui/Button.jsx
+++ techdesk-frontend/src/components/ui/Button.jsx
@@ -14,7 +14,7 @@ const Button = ({ children, onClick, type = 'button' }) => {
         <button
             type={type}
             onClick={onClick}
-            className="mt-8 w-[268px] h-[40px] bg-gradient-to-r from-[#5D1ACE] to-[#2F0D68] justify-center items-center flex text-white font-bold py-1 px-4 rounded-3xl hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-purple-500 transition-all duration-300"
+            className=" mt-8 w-[268px] h-[40px] bg-gradient-to-r from-[#5D1ACE] to-[#2F0D68] justify-center items-center flex text-white font-bold py-1 px-4 rounded-3xl hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-purple-500 transition-all duration-300"
         >
             {children}
         </button>
--- techdesk-frontend/src/pages/RedefinicaoDeSenha.jsx
+++ techdesk-frontend/src/pages/RedefinicaoDeSenha.jsx
@@ -7,12 +7,16 @@ const RedefinicaoDeSenha = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
-  const [message, SetMessage] = useState
+  const [message, SetMessage] = useState("");
 
-  const handleLogin = async (e) => {};
+  const handlePasswordReset = async (e) => {
+    e.preventDefault();
+    setError("");
+    SetMessage("");
+  };
 
   return (
-    <div className="min-h-screen bg-gradient-to-t from-[#2F0D68] to-[#5D1ACE] flex items-center justify-center p-4">
+    <div className="min-h-screen bg-gradient-to-t from-[#2F0D68] to-[#5D1ACE] flex items-center justify-center p-4 ">
       <div
         className="p-1 rounded-2xl shadow-2xl shadow-black/50"
         style={{
@@ -20,52 +24,53 @@ const RedefinicaoDeSenha = () => {
             "repeating-conic-gradient(from 0deg, #000 0deg 25deg, transparent  0deg 30deg)",
         }}
       >
-        <div className="bg-[#262626] p-8 rounded-lg w-[500px] transition-shadow shadow-2xl shadow-black/50">
-          <div>
-            <h1 className="text-[#FFFFFF] mb-10 text-center text-[24px] font-bold opacity-65">
+        <div className="bg-[#262626]  p-8 rounded-xl w-full max-w-md shadow-2xl shadow-black/50">
+          <div className="text-center mb-8">
+            <h1 className="text-white text-2xl font-bold mb-6">
               Redefinição de Senha
             </h1>
 
-            <form className="space-y-3" onSubmit={handleLogin}>
-              <div>
-                <Input
-                  className="w-44 h-44"
-                  type="email"
-                  placeholder="Digite seu usuário..."
-                  aria-label="Usuário"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </div>
-
-              <div>
-                <Input
-                  type="email"
-                  placeholder="Digite seu email..."
-                  aria-label="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-
-                <div className="flex justify-end mt-1">
-                  <a
-                    href="#"
-                    className="text-[#9067D7] text-[10px] sm:text-xs font-semibold hover:underline"
-                  >
-                    Esqueci minha senha
-                  </a>
-                </div>
-              </div>
-
-              {error && (
-                <p className="text-red-400 text-sm text-center">{error}</p>
-              )}
-
-              <div>
-                <Button>Recuperar Senha</Button>
-              </div>
-            </form>
+            <p className="text-[#7C7E8B] mt-2 text-[9px] sm:text-[11px] font-semibold text-center opacity-50">
+              Digite seu usuário e endereço de e-mail e lhe enviaremos um link
+              para redefinição de senha
+            </p>
           </div>
+
+          <form className="space-y-6" onSubmit={handlePasswordReset}>
+            <div>
+              <Input
+                type="text"
+                placeholder="Digite seu usuário..."
+                aria-label="Usuário"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <Input
+                type="email"
+                placeholder="Digite seu e-mail..."
+                aria-label="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            {error && (
+              <p className="text-red-400 text-sm text-center">{error}</p>
+            )}
+
+            {/* Exibição de Mensagem de Sucesso */}
+            {message && (
+              <p className="text-green-400 text-sm text-center">{message}</p>
+            )}
+
+            {/* Botão de Ação */}
+            <div className="ml-14">
+              <Button type="submit">Recuperar senha</Button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
